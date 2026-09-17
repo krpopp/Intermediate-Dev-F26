aac2b8ad87219ad97885dd6927e30ed0417f3cc4
krpopp <kp1640@nyu.edu>
2026-09-17 13:38:47 -0400

    commenting week 2

--- Week 2/Assets/Scripts/BallMove.cs
+++ Week 2/Assets/Scripts/BallMove.cs
@@ -4,12 +4,15 @@ using UnityEngine.InputSystem;
 public class BallMove : MonoBehaviour
 {
 
+    //reference to the ball's rigid body
     Rigidbody2D myBody;
+    //reference to input we'll be listening for
     InputAction jump;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //setting the rigidbody and input vars
         myBody = GetComponent<Rigidbody2D>();
         jump = InputSystem.actions.FindAction("Jump");
         //myBody.AddForceY(500f);
@@ -19,8 +22,10 @@ public class BallMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if any input assigned to the "jump" action is pressed
         if (jump.IsPressed())
         {
+            //add an upwards force to the ball
             myBody.AddForceY(500f);
         }
     }
